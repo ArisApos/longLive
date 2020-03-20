4bd3c917b04409b45145ae4c9156a2ea578830f5
Aris <arisapostoloy@gmail.com>
2020-03-20 19:12:37 +0200

    organize objects

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,9 @@
 import { changeProductContainer, onKeyUp } from './actions'
 import { searchedProducts, suggestedProducts } from './reducers'
-import { products, brands, types } from './data'
+import { products, brands, types, prices } from './data'
 
 export {
-  changeProductContainer,
-  onKeyUp,
-  products,
-  searchedProducts,
-  suggestedProducts,
-  brands,
-  types
+  changeProductContainer, onKeyUp,
+  products, brands, types, prices,
+  searchedProducts, suggestedProducts
 }
