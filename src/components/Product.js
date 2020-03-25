7965f72128e7b55136f0cd4e2c984b56f5c50d1e
Aris <arisapostoloy@gmail.com>
2020-03-25 02:25:08 +0200

    Increase minimalism

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Product = ({ name, type, brand, price, imageSrc }) => (
   <div className="center-text">
-    <h3>{ name }</h3>
+    <div className="productName">{ name }</div>
     <img src={ imageSrc } alt="img" height="250" width="170" className="center"/>
     <div>Brand: { brand }</div>
     <div>Style: { type }</div>
